9113b97e84aaa715e660250e7717a8247a327e9e
ZionYou <peter832916@gmail.com>
2023-06-06 20:43:17 +0800

    feat: add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import './App.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage, LoginPage, RegistPage, AdminLoginPage } from "pages"
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
-      <LoginPage />
-      <RegistPage />
-      <AdminLoginPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="regist" element={<RegistPage />} />
+          <Route path="adminLogin" element={<AdminLoginPage />} />
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
+    
   );
 }
 
